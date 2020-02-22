482647f053c7473ad717db5e78d24ba401f30b5d
Niljak <ncfjakobsen@me.com>
2020-02-22 21:25:49 +0100

    feat: added material ui design components
    * fix: missing styles import
    
    * chore: corrected editor config for tsx
    
    * chore: eslint and editorconfig updates
    
    * Added material-ui components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 
 export const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="app-container">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className="title" variant="h6">
+            The Devils Backbone
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
